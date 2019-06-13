417b9b5e812d14a89d27ecd6cdf353367b81d6e1
b123400 Chan <i@b123400.net>
2019-06-13 20:11:19 +0900

    Get SDK

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,3 +1,10 @@
 # This file is executed on app center
 
 Move-Item -Force -Path $env:APPCENTER_SOURCE_DIRECTORY\NuGet.AppCenter.config -Destination $env:APPCENTER_SOURCE_DIRECTORY\NuGet.config
+
+# Get chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install windows-sdk-8.1 wixtoolset
+
+PowerShell.exe -ExecutionPolicy Bypass ` -File download.ps1
