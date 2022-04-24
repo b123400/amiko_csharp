77f4d706ca7887920f42c5c6b33052ffdba02a39
b123400 <i@b123400.net>
2022-04-24 22:18:57 +0900

    temp

--- AmiKoWindows/PreBuild.bat
+++ AmiKoWindows/PreBuild.bat
@@ -1,12 +1,24 @@
 REM Make sure this file is encoded as UTF-8 (no BOM)
 
+ECHO "One is %1"
+ECHO "Two is %2"
+
+ECHO "List dir1:"
+DIR "%1\AmiKoWindows\Properties\"
+
 SET resxfile="%1\AmiKoWindows\Properties\Resources.resx"
 IF EXIST %resxfile% (
   ECHO --- Deleting resource file ---
   DEL %resxfile%
 )
 
+ECHO "List dir2:"
+DIR "%1\AmiKoWindows\Properties\"
+
 ECHO --- Copying resource file ---
 
 :: copy {de|fr}.resx to resx
 (COPY /Y "%1\AmiKoWindows\Properties\%2" "%1\AmiKoWindows\Properties\Resources.resx")
+
+ECHO "List dir3:"
+DIR "%1\AmiKoWindows\Properties\"
